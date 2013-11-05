bf3bdefb348fb4bc5b0ed283a31e347fadaf0b29
helix-dan <lotus.bat@gmail.com>
2013-11-05 14:28:18 +0800

    fix http

--- lib/server_request/send_sign.rb
+++ lib/server_request/send_sign.rb
@@ -38,7 +38,7 @@ module ServerRequest
     end
 
     def post_sync
-      http = Net::Http.new(@host + @uri)
+      http = Net::HTTP.new(@host + @uri)
       http.post(@params_hash)
     end
 
@@ -50,8 +50,6 @@ module ServerRequest
     end
 
     def get_sync
-    	# http = Net::Http.new(@host + @uri)
-    	# http.get
       Net::HTTP.get(URI.parse(@host + @uri))
     end
 
